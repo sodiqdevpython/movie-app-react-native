309d1a589fe9f78f84710dcbfc451289490cfdf1
sodiqdevpython <sodiqdevpython@gmail.com>
2023-12-09 18:17:11 +0500

    integrated with tmdb api

--- src/navigation/navigations.js
+++ src/navigation/navigations.js
@@ -2,7 +2,7 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from '../screens/home';
+import HomeScreen from '../screens/home';
 import Detail from '../screens/detail';
 
 const Stack = createNativeStackNavigator();
@@ -11,7 +11,7 @@ export default function Navigation() {
   return (
     <NavigationContainer>
         <Stack.Navigator>
-            <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+            <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
             <Stack.Screen name="Detail" component={Detail} options={{ headerShown: false }} />
         </Stack.Navigator>
     </NavigationContainer>
--- src/screens/home.js
+++ src/screens/home.js
@@ -1,38 +1,90 @@
-import { View, Image, StyleSheet, SafeAreaView, Text, ScrollView } from 'react-native'
-import React from 'react'
+import { Dimensions, View, Button,StyleSheet, Image, ScrollView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React, { useEffect, useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { LinearGradient } from 'expo-linear-gradient';
-import { AntDesign } from '@expo/vector-icons';
+import { MagnifyingGlassIcon } from 'react-native-heroicons/outline';
 
-export default function Home({ navigation }) {
+import Carousel from 'react-native-snap-carousel';
+
+
+
+export default function HomeScreen({ navigation }) {
+  const width = Dimensions.get('window').width;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        const request = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=f391c404d0c20bc7c6293f8cc766ee2e');
+        const response = await request.json();
+        setData(response.results.map(movie => ({
+          imageUrl: `https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`
+        })));
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getData();
+  }, []);
+
+  const renderItem = ({ item }) => (
+    <View style={style.carouselItem}>
+      <Image source={{ uri: item.imageUrl }} style={style.image} />
+    </View>
+  );
+
   return (
-    <LinearGradient style={{flex: 1}} colors={['#33363b', '#1e293b' ,'#020617']}>
-      <View style={style.head}>
-        <SafeAreaView style={{flex: 1, flexDirection: 'row', alignItems: 'flex-end'}}>
-          <View style={{flex: 1, paddingLeft: '5%' }}>
-            <Image source={require('../../assets/logo.png')} />
-          </View>
-          <View style={{flex: 1, alignItems: 'flex-end', paddingRight: '6%'}}>
-            <AntDesign name="search1" size={32} color="white" />
+    <LinearGradient style={{ flex: 1, justifyContent: 'center' }} colors={['#33363b', '#1e293b', '#020617']}>
+      <View style={style.home}>
+        <StatusBar style='light' />
+        <SafeAreaView style={{ height: 90 }}>
+          <View style={{ flexDirection: 'row' }}>
+            <View style={{ flex: 1 }}>
+              <Image style={style.logo} source={require('../../assets/logo.png')} />
+            </View>
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-end', paddingRight: '10%' }}>
+              <MagnifyingGlassIcon size={30} strokeWidth={3} color={'whitesmoke'} />
+            </View>
           </View>
         </SafeAreaView>
-      </View>
-      
-      <View style={style.body}>
         <ScrollView>
-          
+          <View style={{ flex: 1, paddingTop: '7%' }}>
+            <Carousel
+              data={data}
+              renderItem={renderItem}
+              sliderWidth={width}
+              itemWidth={width * 0.7}
+              firstItem={5}
+              slideStyle={{ display: 'flex' }}
+              inactiveSlideOpacity={0.2}
+            />
+          </View>
         </ScrollView>
       </View>
     </LinearGradient>
-  )
+  );
 }
 
 const style = StyleSheet.create({
-    head: {
-        flex: 1
-    },
-    body: {
-      flex: 10
-    }
-})
+  home: {
+    flex: 1,
+  },
+  logo: {
+    width: 120,
+    height: 80,
+    resizeMode: 'contain',
+    marginLeft: 20
+  },
+  carouselItem: {
+    borderRadius: 20,
+    overflow: 'hidden'
+  },
+  image: {
+    width: '100%',
+    height: 410,
+    resizeMode: 'cover'
+  }
+});
